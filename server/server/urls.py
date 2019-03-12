596dc960a61417ddedc42752e6107b1d6826a9f1
Shorh <Shorhina@gmail.com>
2019-03-12 17:18:12 +0500

    django server

---
+++ server/server/urls.py
@@ -0,0 +1,28 @@
+"""server URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from main import views as main
+
+urlpatterns = [
+    path('', main.main, name='main'),
+    path('catalog/karkasson/', main.karkasson, name='karkasson'),
+    path('catalog/machi-koro/', main.machi_koro, name='machi_koro'),
+    path('catalog/manchkin/', main.manchkin, name='manchkin'),
+    path('catalog/', main.catalog, name='catalog'),
+    path('contacts/', main.contact, name='contacts'),
+    path('admin/', admin.site.urls),
+]
