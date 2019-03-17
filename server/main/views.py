87f0c76ad3ad59854d8ae2cb04e13854ef2c8932
Shorh <Shorhina@gmail.com>
2019-03-17 21:43:40 +0500

    Более корректная работа со статикой

--- server/main/views.py
+++ server/main/views.py
@@ -4,9 +4,9 @@ from collections import namedtuple
 
 def start(request):
     Product = namedtuple('Product', 'name url image_url price')
-    new = [Product('Каркассон', 'karkasson', '/static/products/img/karkasson.jpg', '1 300')]
-    hot = [Product('Мачи Коро', 'machi_koro', '/static/products/img/machi-koro.jpg', '1 000')]
-    sale = [Product('Манчкин', 'manchkin', '/static/products/img/manchkin.jpg', '1 000')]
+    new = [Product('Каркассон', 'karkasson', 'products/img/karkasson.jpg', '1 300')]
+    hot = [Product('Мачи Коро', 'machi_koro', 'products/img/machi-koro.jpg', '1 000')]
+    sale = [Product('Манчкин', 'manchkin', 'products/img/manchkin.jpg', '1 000')]
 
     return render(
         request,
--- server/products/views.py
+++ server/products/views.py
@@ -5,9 +5,9 @@ from collections import namedtuple
 def catalog(request):
     Product = namedtuple('Product', 'name url image_url price')
     products = [
-        Product('Каркассон', 'karkasson', '/static/products/img/karkasson.jpg', '1 300'),
-        Product('Мачи Коро', 'machi_koro', '/static/products/img/machi-koro.jpg', '1 000'),
-        Product('Манчкин', 'manchkin', '/static/products/img/manchkin.jpg', '1 000'),
+        Product('Каркассон', 'karkasson', 'products/img/karkasson.jpg', '1 300'),
+        Product('Мачи Коро', 'machi_koro', 'products/img/machi-koro.jpg', '1 000'),
+        Product('Манчкин', 'manchkin', 'products/img/manchkin.jpg', '1 000'),
     ]
 
     return render(
