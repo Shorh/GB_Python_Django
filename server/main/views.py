607d5c7ec4d0c5444c27beddfb2738e2bc42febe
Shorh <Shorhina@gmail.com>
2019-03-17 21:30:03 +0500

    add context

--- server/main/views.py
+++ server/main/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
+from collections import namedtuple
 
 
 def start(request):
-    return render(request, 'main/index.html')
+    Product = namedtuple('Product', 'name url image_url price')
+    new = [Product('Каркассон', 'karkasson', '/static/products/img/karkasson.jpg', '1 300')]
+    hot = [Product('Мачи Коро', 'machi_koro', '/static/products/img/machi-koro.jpg', '1 000')]
+    sale = [Product('Манчкин', 'manchkin', '/static/products/img/manchkin.jpg', '1 000')]
+
+    return render(
+        request,
+        'main/index.html',
+        {
+            'title': 'Сундук с сокровищами',
+            'link_list': ['main/css/index.css'],
+            'new': new,
+            'hot': hot,
+            'sale': sale,
+        }
+    )
 
 
 def contacts(request):
-    return render(request, 'main/contacts.html')
+    return render(
+        request,
+        'main/contacts.html',
+        {
+            'title': 'Контакты',
+            'link_list': ['main/css/contacts.css'],
+        }
+    )
--- server/products/views.py
+++ server/products/views.py
@@ -1,17 +1,54 @@
 from django.shortcuts import render
+from collections import namedtuple
 
 
 def catalog(request):
-    return render(request, 'products/index.html')
+    Product = namedtuple('Product', 'name url image_url price')
+    products = [
+        Product('Каркассон', 'karkasson', '/static/products/img/karkasson.jpg', '1 300'),
+        Product('Мачи Коро', 'machi_koro', '/static/products/img/machi-koro.jpg', '1 000'),
+        Product('Манчкин', 'manchkin', '/static/products/img/manchkin.jpg', '1 000'),
+    ]
+
+    return render(
+        request,
+        'products/index.html',
+        {
+            'title': 'Каталог',
+            'link_list': [''],
+            'products': products,
+        }
+    )
 
 
 def karkasson(request):
-    return render(request, 'products/karkasson.html')
+    return render(
+        request,
+        'products/karkasson.html',
+        {
+            'title': 'Каркассон',
+            'link_list': ['products/css/product.css'],
+        }
+    )
 
 
 def machi_koro(request):
-    return render(request, 'products/machi-koro.html')
+    return render(
+        request,
+        'products/machi-koro.html',
+        {
+            'title': 'Мачи Коро',
+            'link_list': ['products/css/product.css'],
+        }
+    )
 
 
 def manchkin(request):
-    return render(request, 'products/manchkin.html')
+    return render(
+        request,
+        'products/manchkin.html',
+        {
+            'title': 'Манчкин',
+            'link_list': ['products/css/product.css'],
+        }
+    )
