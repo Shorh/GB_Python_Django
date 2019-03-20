e9c44d06762d2f45d42b3401ed26e66bd540a1d6
Shorh <Shorhina@gmail.com>
2019-03-21 00:39:54 +0500

    Данные по товарам для меню из контекста

--- server/main/views.py
+++ server/main/views.py
@@ -4,23 +4,28 @@ import json
 
 def main(request):
     with open('products/fixtures/data/data.json') as file:
+        data = json.load(file)
         return render(
             request,
             'main/index.html',
             {
                 'title': 'Сундук с сокровищами',
                 'link_list': ['main/css/index.css'],
-                'products': json.load(file),
+                'products': data,
+                'menu': {itm['name']: 0 for itm in data},
             }
         )
 
 
 def contacts(request):
-    return render(
-        request,
-        'main/contacts.html',
-        {
-            'title': 'Контакты',
-            'link_list': ['main/css/contacts.css'],
-        }
-    )
+    with open('products/fixtures/data/data.json') as file:
+        data = json.load(file)
+        return render(
+            request,
+            'main/contacts.html',
+            {
+                'title': 'Контакты',
+                'link_list': ['main/css/contacts.css'],
+                'menu': {itm['name']: 0 for itm in data},
+            }
+        )
