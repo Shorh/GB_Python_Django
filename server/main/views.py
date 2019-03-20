ddf1e0b0c571fc689e7ef10cfdef942b89065f3f
Shorh <Shorhina@gmail.com>
2019-03-21 02:22:33 +0500

    pk=id

--- server/main/views.py
+++ server/main/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from products.models import Product
+from collections import namedtuple
 
 
 def main(request):
     data = Product.objects.all()
+    Menu = namedtuple('Menu', 'name, id')
     return render(
         request,
         'main/index.html',
@@ -11,19 +13,20 @@ def main(request):
             'title': 'Сундук с сокровищами',
             'link_list': ['main/css/index.css'],
             'products': data,
-            'menu': [itm.name for itm in data],
+            'menu': [Menu(itm.name, itm.id) for itm in data],
         }
     )
 
 
 def contacts(request):
     data = Product.objects.all()
+    Menu = namedtuple('Menu', 'name, id')
     return render(
         request,
         'main/contacts.html',
         {
             'title': 'Контакты',
             'link_list': ['main/css/contacts.css'],
-            'menu': [itm.name for itm in data],
+            'menu': [Menu(itm.name, itm.id) for itm in data],
         }
     )
--- server/products/views.py
+++ server/products/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from products.models import Product
+from collections import namedtuple
 
 
 def catalog(request):
     data = Product.objects.all()
+    Menu = namedtuple('Menu', 'name, id')
     return render(
         request,
         'products/index.html',
@@ -11,20 +13,21 @@ def catalog(request):
             'title': 'Каталог',
             'link_list': [''],
             'products': data,
-            'menu': [itm.name for itm in data],
+            'menu': [Menu(itm.name, itm.id) for itm in data],
         }
     )
 
 
 def product_detail(request, pk):
     data = Product.objects.all()
+    Menu = namedtuple('Menu', 'name, id')
     return render(
         request,
         'products/detail.html',
         {
-            'title': data[pk].name,
+            'title': data.get(id=pk).name,
             'link_list': ['products/css/product.css'],
-            'product': data[pk],
-            'menu': [itm.name for itm in data],
+            'product': data.get(id=pk),
+            'menu': [Menu(itm.name, itm.id) for itm in data],
         }
     )
