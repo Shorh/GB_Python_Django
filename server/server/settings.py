0f6e7cee577b941a88ba55e0db105c8c2cdf9600
Shorh <Shorhina@gmail.com>
2019-03-21 01:50:50 +0500

    media

--- server/server/settings.py
+++ server/server/settings.py
@@ -127,3 +127,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'server', 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
