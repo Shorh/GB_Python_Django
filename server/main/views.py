d695357a5492ed40e063719cc082492573218493
Shorh <Shorhina@gmail.com>
2019-03-21 02:11:27 +0500

    json -> models

--- server/main/views.py
+++ server/main/views.py
@@ -1,31 +1,29 @@
 from django.shortcuts import render
-import json
+from products.models import Product
 
 
 def main(request):
-    with open('products/fixtures/data/data.json') as file:
-        data = json.load(file)
-        return render(
-            request,
-            'main/index.html',
-            {
-                'title': 'Сундук с сокровищами',
-                'link_list': ['main/css/index.css'],
-                'products': data,
-                'menu': [itm['name'] for itm in data],
-            }
-        )
+    data = Product.objects.all()
+    return render(
+        request,
+        'main/index.html',
+        {
+            'title': 'Сундук с сокровищами',
+            'link_list': ['main/css/index.css'],
+            'products': data,
+            'menu': [itm.name for itm in data],
+        }
+    )
 
 
 def contacts(request):
-    with open('products/fixtures/data/data.json') as file:
-        data = json.load(file)
-        return render(
-            request,
-            'main/contacts.html',
-            {
-                'title': 'Контакты',
-                'link_list': ['main/css/contacts.css'],
-                'menu': [itm['name'] for itm in data],
-            }
-        )
+    data = Product.objects.all()
+    return render(
+        request,
+        'main/contacts.html',
+        {
+            'title': 'Контакты',
+            'link_list': ['main/css/contacts.css'],
+            'menu': [itm.name for itm in data],
+        }
+    )
--- server/products/views.py
+++ server/products/views.py
@@ -1,32 +1,30 @@
 from django.shortcuts import render
-import json
+from products.models import Product
 
 
 def catalog(request):
-    with open('products/fixtures/data/data.json') as file:
-        data = json.load(file)
-        return render(
-            request,
-            'products/index.html',
-            {
-                'title': 'Каталог',
-                'link_list': [''],
-                'products': data,
-                'menu': [itm['name'] for itm in data],
-            }
-        )
+    data = Product.objects.all()
+    return render(
+        request,
+        'products/index.html',
+        {
+            'title': 'Каталог',
+            'link_list': [''],
+            'products': data,
+            'menu': [itm.name for itm in data],
+        }
+    )
 
 
 def product_detail(request, pk):
-    with open('products/fixtures/data/data.json') as file:
-        data = json.load(file)
-        return render(
-            request,
-            'products/detail.html',
-            {
-                'title': data[pk]['name'],
-                'link_list': ['products/css/product.css'],
-                'product': data[pk],
-                'menu': [itm['name'] for itm in data],
-            }
-        )
+    data = Product.objects.all()
+    return render(
+        request,
+        'products/detail.html',
+        {
+            'title': data[pk].name,
+            'link_list': ['products/css/product.css'],
+            'product': data[pk],
+            'menu': [itm.name for itm in data],
+        }
+    )
