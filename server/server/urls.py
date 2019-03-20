739bc629bab4d97b4f2ffd85e5fd269b8ed540ad
Shorh <Shorhina@gmail.com>
2019-03-21 02:11:03 +0500

    Использование MEDIA

--- server/server/urls.py
+++ server/server/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import (
+    path, include)
 
 
 urlpatterns = [
@@ -22,3 +25,6 @@ urlpatterns = [
     path('catalog/', include('products.urls')),
     path('', include('main.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
