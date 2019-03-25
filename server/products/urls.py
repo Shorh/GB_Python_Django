9afcfbe82b06abc9dc816493a1e272a22a8a0311
Shorh <Shorhina@gmail.com>
2019-03-25 17:59:44 +0500

    создание категории

--- server/products/urls.py
+++ server/products/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import (
-    catalog, product_detail
+    catalog, product_detail, category_create
 )
 
 
 app_name = 'products'
 
 urlpatterns = [
+    path('category_create/', category_create, name='category_create'),
     path('<int:pk>/', product_detail, name='detail'),
     path('', catalog, name='main'),
 ]
