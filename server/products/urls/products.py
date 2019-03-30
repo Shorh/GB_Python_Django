88087533dc929467014e643b461d97a396e38fbe
Shorh <Shorhina@gmail.com>
2019-03-30 18:46:02 +0500

    change name view

--- server/products/urls/products.py
+++ server/products/urls/products.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from products.views import (
-    catalog, product_detail, product_create, product_update, product_delete
+    product_list, product_detail, product_create, product_update, product_delete
 )
 
 
@@ -11,5 +11,5 @@ urlpatterns = [
     path('update/<int:pk>/', product_update, name='update'),
     path('delete/<int:pk>/', product_delete, name='delete'),
     path('<int:pk>/', product_detail, name='detail'),
-    path('', catalog, name='main'),
+    path('', product_list, name='main'),
 ]
