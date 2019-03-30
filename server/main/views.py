711a8927637a748bfa9c04b8da96efe6d3736f15
Shorh <Shorhina@gmail.com>
2019-03-30 18:45:41 +0500

    use Menu

--- server/main/views.py
+++ server/main/views.py
@@ -1,32 +1,27 @@
 from django.shortcuts import render
-from products.models import Product
-from collections import namedtuple
+from server.menu import Menu, products
 
 
 def main(request):
-    data = Product.objects.all()
-    Menu = namedtuple('Menu', 'name, id')
     return render(
         request,
         'main/index.html',
         {
             'title': 'Сундук с сокровищами',
             'link_list': ['main/css/index.css'],
-            'products': data,
-            'menu': [Menu(itm.name, itm.id) for itm in data],
+            'products': products,
+            'menu': Menu,
         }
     )
 
 
 def contacts(request):
-    data = Product.objects.all()
-    Menu = namedtuple('Menu', 'name, id')
     return render(
         request,
         'main/contacts.html',
         {
             'title': 'Контакты',
             'link_list': ['main/css/contacts.css'],
-            'menu': [Menu(itm.name, itm.id) for itm in data],
+            'menu': Menu,
         }
     )
