abae106eae3bde4402d3749ea10a913cd9885aea
Shorh <Shorhina@gmail.com>
2019-03-30 19:00:19 +0500

    menu is done!

--- server/server/menu.py
+++ server/server/menu.py
@@ -4,5 +4,7 @@ from products.models import Product, ProductCategory
 
 categories = ProductCategory.objects.all()
 products = Product.objects.all()
-Menu = namedtuple('Menu', 'name, id')
-Menu = [Menu(itm.name, itm.id) for itm in categories]
+Cat = namedtuple('Cat', 'name, id')
+Prod = namedtuple('Prod', 'name, id, category_id')
+menu_categories = [Cat(itm.name, itm.id) for itm in categories]
+menu_products = [Prod(itm.name, itm.id, itm.category_id) for itm in products]
