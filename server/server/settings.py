6e7e2bfe37b2dc62ab9e8da59b64f2a8ddb6f4a9
Shorh <Shorhina@gmail.com>
2019-04-07 19:53:45 +0500

    '/' ->  reverse_lazy('main:main')

--- server/server/settings.py
+++ server/server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'accounts.AccountUser'
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = reverse_lazy('main:main')
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
