1625ba4a59870d01d5fd5df5524c94b9661db060
Shorh <Shorhina@gmail.com>
2019-04-07 18:31:25 +0500

    admin

--- server/products/admin.py
+++ server/products/admin.py
@@ -4,32 +4,109 @@ from django.template.loader import render_to_string
 from .models import Product, ProductCategory
 
 
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = [
-        'category',
         'name',
+        'category',
         'picture',
         'price_now',
         'price_old',
         'quantity',
         'status',
+        'created',
+        'modified',
     ]
 
     list_filter = [
         'category',
-        'name',
         'price_now',
         'price_old',
         'quantity',
         'status',
+        'created',
+        'modified',
+    ]
+
+    search_fields = [
+        'name',
+        'description',
+    ]
+
+    fieldsets = (
+        (
+            None, {
+                'fields': (
+                    'name',
+                    'category',
+                    'status',
+                    'price_now',
+                    'price_old',
+                    'quantity',
+                )
+            }
+        ),
+        (
+            'Описание', {
+                'fields': (
+                    'short_description',
+                    'description',
+                    'specifications',
+                    'image',
+                )
+            }
+        )
+    )
+
+    def picture(self, obj):
+        return render_to_string(
+            'products/components/picture.html',
+            {'object': obj}
+        )
+
+
+class ProductInline(admin.TabularInline):
+    model = Product
+
+
+@admin.register(ProductCategory)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'picture',
+        'product_num',
+        'description',
+        'created',
+        'modified',
+    ]
+
+    list_filter = [
+        'created',
+        'modified',
+    ]
+
+    search_fields = ['name']
+
+    fieldsets = (
+        (
+            None,
+            {'fields': ('name',)}
+        ),
+        (
+            'Описание',
+            {'fields': ('description',)}
+        )
+    )
+
+    inlines = [
+        ProductInline
     ]
 
     def picture(self, obj):
         return render_to_string(
             'products/components/picture.html',
-            {'image': obj.image}
+            {'object': obj}
         )
 
-
-admin.site.register(Product, ProductAdmin)
-admin.site.register(ProductCategory)
+    def product_num(self, obj):
+        return obj.category.count()
