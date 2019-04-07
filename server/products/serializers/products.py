e772bef98432ad0119280934ff08d62e21f1d9ef
Shorh <Shorhina@gmail.com>
2019-04-07 21:02:47 +0500

    serializer for Product

--- server/products/serializers/products.py
+++ server/products/serializers/products.py
@@ -1,13 +1,17 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from products.models import Product
 
 
 class ProductSerializer(ModelSerializer):
+    category = SerializerMethodField()
+    is_pure = SerializerMethodField()
+
     class Meta:
         model = Product
         fields = [
-            'category',
+            'url',
             'name',
+            'category',
             'short_description',
             'description',
             'specifications',
@@ -16,4 +20,14 @@ class ProductSerializer(ModelSerializer):
             'quantity',
             'image',
             'status',
+            'is_pure',
+            'created',
+            'modified',
         ]
+
+    def get_category(self, obj):
+        if obj.category:
+            return obj.category.name
+
+    def get_is_pure(self, obj):
+        return obj.created == obj.modified
