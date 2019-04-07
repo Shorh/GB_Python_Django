46f85436811182d2d73e5f0fe518b511f966f7e5
Shorh <Shorhina@gmail.com>
2019-04-07 15:33:06 +0500

    admin, products -> свое отображение админ-панели с изображениями

--- server/products/admin.py
+++ server/products/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
-from .models import (
-    Product, ProductCategory
-)
+from django.template.loader import render_to_string
+
+from .models import Product, ProductCategory
 
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+            'category',
+            'name',
+            'price_now',
+            'price_old',
+            'quantity',
+            'picture',
+            'status',
+        ]
+
+    def picture(self, obj):
+        return render_to_string(
+            'products/components/picture.html',
+            {'image': obj.image}
+        )
+
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductCategory)
