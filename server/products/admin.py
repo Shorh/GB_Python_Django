bead9177d9f0c5cb989f357fb1fc3fcd9fd4ed0a
Shorh <Shorhina@gmail.com>
2019-04-07 15:37:45 +0500

    add list_filter

--- server/products/admin.py
+++ server/products/admin.py
@@ -6,14 +6,23 @@ from .models import Product, ProductCategory
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = [
-            'category',
-            'name',
-            'price_now',
-            'price_old',
-            'quantity',
-            'picture',
-            'status',
-        ]
+        'category',
+        'name',
+        'picture',
+        'price_now',
+        'price_old',
+        'quantity',
+        'status',
+    ]
+
+    list_filter = [
+        'category',
+        'name',
+        'price_now',
+        'price_old',
+        'quantity',
+        'status',
+    ]
 
     def picture(self, obj):
         return render_to_string(
