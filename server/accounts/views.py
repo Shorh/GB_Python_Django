59c40740341c2d49450d6876baf553995ba97931
Shorh <Shorhina@gmail.com>
2019-04-07 19:55:32 +0500

    del unused

--- server/accounts/views.py
+++ server/accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth import login
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
 from products.models import ProductCategory
